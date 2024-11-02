1abf47f0377ed715e535f489a52060de7be7a8f3
Pooya Fekri <pooyafekri79@gmail.com>
2024-11-02 16:38:32 +0330

    Change sa config

--- zex_deposit/sa/config.py
+++ zex_deposit/sa/config.py
@@ -19,17 +19,17 @@ INFURA_KEY = os.environ["INFURA_KEY"]
 USER_DEPOSIT_FACTORY_ADDRESS = os.environ["USER_DEPOSIT_FACTORY_ADDRESS"]
 USER_DEPOSIT_BYTECODE_HASH = os.environ["USER_DEPOSIT_BYTECODE_HASH"]
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://127.0.0.1:27017/")
-BATCH_BLOCK_NUMBER_SIZE = int(os.getenv("BATCH_BLOCK_NUMBER_SIZE", 3))
-MAX_DELAY_PER_BLOCK_BATCH = int(os.getenv("MAX_DELAY_PER_BLOCK_BATCH", 10))
+BATCH_BLOCK_NUMBER_SIZE = int(os.getenv("BATCH_BLOCK_NUMBER_SIZE", 5))
+MAX_DELAY_PER_BLOCK_BATCH = int(os.getenv("MAX_DELAY_PER_BLOCK_BATCH", 3))
 
-DKG_JSON_PATH = os.getenv("DKG_JSON_PATH", "./sa/dkgs.json")
+DKG_JSON_PATH = os.getenv("DKG_JSON_PATH", "./dkgs/dkgs.json")
 DKG_NAME = os.getenv("DKG_NAME", "ethereum")
 
 CHAINS_CONFIG = {
     "11155111": ChainConfig(
         private_rpc="https://ethereum-sepolia-rpc.publicnode.com",
         chain_id=ChainId(11155111),
-        from_block=6991486,
+        from_block=6995672,
     )
 }
 
