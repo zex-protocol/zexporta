294c16ff617f7ae7bf97de83a07c2a0a101b6f28
Pooya Fekri <pooyafekri79@gmail.com>
2024-12-09 22:23:36 +0330

    New deposit style

---
+++ zex_deposit/db/chain.py
@@ -0,0 +1,26 @@
+from zex_deposit.custom_types import BlockNumber, ChainId
+
+from .database import chain_collection
+
+
+async def upsert_chain_last_observed_block(
+    chain_id: ChainId, block_number: BlockNumber | int
+):
+    query = {"chainId": chain_id.value}
+    update = {
+        "$set": {
+            "last_observed_block": block_number,
+        }
+    }
+
+    result = await chain_collection.update_one(query, update, upsert=True)
+    return result
+
+
+async def get_last_observed_block(chain_id: ChainId) -> BlockNumber | None:
+    query = {"chainId": chain_id.value}
+    result = await chain_collection.find_one(query)
+    if result:
+        return result["last_observed_block"]
+    else:
+        return None
