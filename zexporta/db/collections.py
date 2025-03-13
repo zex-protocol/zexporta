b074c7d7476a08da40c84f21e298586133b972d2
ASHKAN <sijalkhf@gmail.com>
2025-03-13 12:07:10 +0330

    add comments

--- zexporta/db/collections.py
+++ zexporta/db/collections.py
@@ -2,7 +2,9 @@ import os
 
 from pymongo import AsyncMongoClient
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://mongodb:27017/")  # noqa: B904 FIXME
+MONGO_URI = os.getenv(
+    "MONGO_URI", "mongodb://mongodb:27017/"
+)  # FIXME: due to circular import, We must move this config to configs in future
 
 client = AsyncMongoClient(MONGO_URI)
 db = client["transaction_database"]
